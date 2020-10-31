4b8ac393674b125e90c311002f33413ef2f8da46
Alaple <lucas.palacios@southworks.com>
2020-10-31 13:08:28 -0300

    Add project files.

---
+++ TP/Contexto/TPBaseDatosContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP.Contexto
+{
+    public class TPBaseDatosContext: DbContext
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        [Display(Name = "Fecha inscripción")]
+        public DateTime FechaInscripto { get; set; }
+        [EnumDataType(typeof(Deporte))]
+        public Deporte DeporteFavorito { get; set; }
+    }
+}
